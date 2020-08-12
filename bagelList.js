0657ab0fc79aa929a3b2e3b78e0cee9351779a25
Jessica Pope <jessica.pope.co@gmail.com>
2020-08-12 16:06:17 -0600

    Add Modules

---
+++ bagelList.js
@@ -0,0 +1,13 @@
+import {createDeleteButton} from './delete.js'
+
+const bagelUl = document.getElementById('bagel-ul')
+
+function renderBagelType(bagel, id){
+    const li = document.createElement('li')
+    li.innerText = bagel
+    li.id = id
+    bagelUl.appendChild(li)
+    createDeleteButton(li)
+}
+
+export {renderBagelType}
